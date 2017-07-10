a149944429c56e40f4d7ea483fe3be9d5af82fdb
alita <alitakendrick@Alitas-MacBook-Pro.local>
2017-07-10 15:20:40 -0500

    a little styling

--- app/components/About.js
+++ app/components/About.js
@@ -8,14 +8,15 @@ class About extends React.Component {
         <div className='row'>
           <div className='col-md-6'>
             <h3>Contact Me</h3>
-            <button type="button" className="btn btn-default btn-primary col-md-4">Email</button> <br /> <br />
+            <button type="button" className="btn btn-default btn-primary col-md-4"> <a href="mailto:alitakendrick121@gmail.com?Subject=Portfolio%20" target="_top">Email </a></button> <br /> <br />
             <button type="button" className="btn btn-default btn-primary col-md-4">GitHub</button> <br /> <br />
             <button type="button" className="btn btn-default btn-primary col-md-4">LinkedIn</button> <br /> 
           </div>
           <div className='col-md-6'>
             <h3>About Me</h3>
-            <p> text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text
-            </p>            
+            <img src="./dist/images/head-shot.gif" height="150" width="150"/>
+            <p>Hello! My name is Alita and I am a junior web developer. </p>
+             
           </div>
         </div>
       </div>
--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -5,11 +5,11 @@ class Portfolio extends React.Component {
   render() {
     return (
      <div className='portfolio' style={{textAlign: 'center'}}>
-        <h1>Portfolio text</h1>
+        <h1>My Projects</h1>
 
         <div className='row'>
           <div className='col-md-4'>
-            <div class="thumbnail">
+            <div className="thumbnail portfolio-project">
               <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
               <div className="caption">
                 <h3>Project Name</h3>
@@ -19,22 +19,57 @@ class Portfolio extends React.Component {
             </div>
           </div>
           <div className='col-md-4'>
-            <p> group 2 </p>
+            <div className="thumbnail portfolio-project">
+              <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
+              <div className="caption">
+                <h3>Project Name</h3>
+                <p>Project Description</p>
+                <p><a href="#" className="btn btn-primary" role="button">View Project</a></p>
+              </div>
+            </div>
           </div>
           <div className='col-md-4'>
-            <p> group 3 </p>
+            <div className="thumbnail portfolio-project">
+              <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
+              <div className="caption">
+                <h3>Project Name</h3>
+                <p>Project Description</p>
+                <p><a href="#" className="btn btn-primary" role="button">View Project</a></p>
+              </div>
+            </div>
           </div>
         </div>
-
+        <br />
         <div className='row'>
           <div className='col-md-4'>
-            <p> group 4 </p>
+            <div className="thumbnail portfolio-project">
+              <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
+              <div className="caption">
+                <h3>Project Name</h3>
+                <p>Project Description</p>
+                <p><a href="#" className="btn btn-primary" role="button">View Project</a></p>
+              </div>
+            </div>
           </div>
           <div className='col-md-4'>
-            <p> group 5 </p>
+            <div className="thumbnail portfolio-project">
+              <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
+              <div className="caption">
+                <h3>Project Name</h3>
+                <p>Project Description</p>
+                <p><a href="#" className="btn btn-primary" role="button">View Project</a></p>
+              </div>
+            </div>
           </div>
           <div className='col-md-4'>
-            <p> group 6 </p>
+            <div className="thumbnail portfolio-project">
+              <img src="https://pixy.org/images/placeholder.png" height="134" width="180"/>
+              <div className="caption">
+                <h3>Project Name</h3>
+                <p>Project Description</p>
+                <p><a href="#" className="btn btn-primary" role="button">View Project</a></p>
+              </div>
+            </div>
           </div>
         </div>
 
